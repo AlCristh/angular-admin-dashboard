b3e27cbbb208d2e461d0259eeb0c468ae1151eb5
AlCristh <a.cristhh@gmail.com>
2026-01-07 19:02:37 -0300

    chore: setup Firebase Firestore with Angular 19 standalone

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKtqVSTgnBfWu9rWk8ZwoDI04uP-PVcBo",
+    authDomain: "dashboard-adm-8f23e.firebaseapp.com",
+    projectId: "dashboard-adm-8f23e",
+    storageBucket: "dashboard-adm-8f23e.firebasestorage.app",
+    messagingSenderId: "1092889834708",
+    appId: "1:1092889834708:web:c793ffc80755275a6af741"
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from './environments/environment';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ]
+}).catch(err => console.error(err));
